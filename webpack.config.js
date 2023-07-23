4629ee08083fef33a8f9a99cee789bca0c93ef01
Adam Robson <adamray312@gmail.com>
2023-07-23 09:53:13 -0700

    project initialized

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const webpack = require('webpack');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname + '/build'),
+    filename: '[name].[contenthash].js',
+    clean: true
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: true
+  },
+   plugins: [
+    new HtmlWebpackPlugin({
+       template: path.join(__dirname, './src/index.html')
+    }),
+    new webpack.ProvidePlugin({
+      React: 'react'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              postcssOptions: {
+                plugins: [
+                  require('postcss-import')(),
+                  require('autoprefixer')()
+                ]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpeg|jpg|png|svg|gif)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      },
+    ]
+  },
+   resolve: {
+    extensions: ['.*', '.js', '.jsx'], //  '.ts', '.tsx'
+  },
+};
