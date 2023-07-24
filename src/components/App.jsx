f8c88fa2e09f8fa2732919fe2cb73df72af4e2fa
Adam Robson <adamray312@gmail.com>
2023-07-24 15:22:58 -0700

    add initial message popup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,18 @@ import GridBoard from './components/GridBoard'
 import NextBlock from './components/NextBlock'
 import ScoreBoard from './components/ScoreBoard'
 import Controls from './components/Controls'
-
+import MessagePopup from './components/MessagePopup'
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Tetris Redux</h1>
+        <h1 className="App-title">Tetris with Redux</h1>
       </header>
       <GridBoard />
       <NextBlock />
       <ScoreBoard />
       <Controls />
+      <MessagePopup />
     </div>
   )
 }
