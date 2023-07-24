52aca59311f406cb4a537e8c7be153cddd63228d
Adam Robson <adamray312@gmail.com>
2023-07-24 14:56:00 -0700

    added initial next block

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import GridBoard from './components/GridBoard'
+import React from 'react'
+import './App.css'
 
-function App() {
+import GridBoard from './components/GridBoard'
+import NextBlock from './components/NextBlock'
+
+export default function App () {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Tetris Redux</h1>
       </header>
       <GridBoard />
+      <NextBlock />
     </div>
-  );
+  )
 }
-
-export default App;
