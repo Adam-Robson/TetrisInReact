b0900beb4d07ccf8e083d4e7bc898e2a1d94693a
Adam Robson <adamray312@gmail.com>
2023-07-24 21:40:13 -0700

    add utils functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -185,3 +185,54 @@ export function defaultGameState() {
     gameOver: false
   }
 }
+
+/*
+player must know if a move is available.
+function needed to determine if current
+block can make a move to new x, y,
+and/or rotate. function parameters =
+shape, grid, x, y, rotation.
+also determine if move will make block
+overlap board edges or another block.
+return true if possible, and false if not.
+
+also given the shape and current
+rotation need to be able to easily
+find the next rotation of a shape.
+*/
+
+export function nextRotation(shape, rotation) {
+  return (rotation + 1) % shapes[shape].length
+}
+
+export function canMoveTo(shape, grid, x, y, rotation) {
+  const currentShape = shapes[shape][rotation]
+  // Loop through all rows and cols of the **shape**
+  for (let row = 0; row < currentShape.length; row++) {
+    for (let col = 0; col < currentShape[row].length; col++) {
+      // Look for a 1 here
+      if (currentShape[row][col] !== 0) {
+        // x offset on the grid
+        const proposedX = col + x
+        // y offset on the grid
+        const proposedY = row + y
+        if (proposedY < 0) {
+          continue
+        }
+        // Get the row on the grid
+        const possibleRow = grid[proposedY]
+        // Check row exists
+        if (possibleRow) {
+          // Check if this column in the row is undefined if it's off the edges, 0, and empty
+          if (possibleRow[proposedX] === undefined || possibleRow[proposedX] !== 0) {
+            // undefined or not 0 and it's occupied we can't move here.
+            return false
+          }
+        } else {
+          return false
+        }
+      }
+    }
+  }
+  return true
+}
